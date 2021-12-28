88d0a59b2b7a0f669225610ff86ca4f869bb54e7
m34m1 <melikamac@gmail.com>
2021-12-28 23:46:43 +0300

    if-else

---
+++ if-else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın");
+            else if (time <= 18)
+                Console.WriteLine("İyi günler");
+            else
+                Console.WriteLine("İyi geceler");
+
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
