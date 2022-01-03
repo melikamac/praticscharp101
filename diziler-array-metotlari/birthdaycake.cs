065a17a8d0c179c2f0d91c3e1f0601111b4997c9
m34m1 <melikamac@gmail.com>
2022-01-03 23:27:07 +0300

    Hackerrank - Birthdaycake Candles

---
+++ diziler-array-metotlari/birthdaycake.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int max = candles.Max();
+        int sucCandle = 0;
+        for (int i = 0; i < candles.Count(); i++)
+        {
+            if (candles[i] == max)
+            {
+                sucCandle += 1;
+            }
+        }
+        return sucCandle;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
