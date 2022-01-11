ad4757853c4c1639054800d546cccdee5dafff79
m34m1 <melikamac@gmail.com>
2022-01-11 22:11:35 +0300

    metotlar

---
+++ metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donuctipi metot_adi(parametreListesi/arguman)
+            //{
+            //komutlar;
+            //return;
+            //};
+
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine("Toplam:" + (a + b));
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(sonuc2.ToString());
+            ornek.EkranaYazdir((a + b).ToString());
+
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
