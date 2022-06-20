c4b587884aba8835110ea4c3899d91819045cf67
MelikAmac <melikamac@gmail.com>
2022-06-20 14:30:12 +0300

    create arraylist

---
+++ array-list/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace array_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Melik");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('M');
+
+            //array list verilerine erişim
+            Console.WriteLine(liste[1]);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(liste);
+            }
+
+        }
+    }
+}
