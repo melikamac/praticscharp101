80cec4356f43231157bd8ed9c2a69abfa9d9d62d
MelikAmac <melikamac@gmail.com>
2022-09-05 13:34:56 +0300

    soru 1 açıklama

--- koleksiyonlarla-algoritma-sorulari/soru-1/Program.cs
+++ koleksiyonlarla-algoritma-sorulari/soru-1/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+// Negatif ve numeric olmayan girişleri engelleyin.
+// Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+// Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
 
 using System.Collections;
 using System.Text.RegularExpressions;
