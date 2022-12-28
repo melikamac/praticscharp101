1b28f85df2d9213013b19572b18e6c2858dedffb
MelikAmac <melikamac@gmail.com>
2022-12-28 16:03:59 +0300

    create project

---
+++ sinif-kavrami/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
