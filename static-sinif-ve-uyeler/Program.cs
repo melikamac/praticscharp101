a84ef032a4f46611895c7fb6c15d3cf426d90740
MelikAmac <melikamac@gmail.com>
2023-02-07 15:16:24 +0300

    static sınıf ve üyeler

---
+++ static-sinif-ve-uyeler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+            Calisan calisan = new Calisan("Melik", "Amaç", "IT");
+            Calisan calisan2 = new Calisan("Melik", "Amaç", "IT");
+            Calisan calisan3 = new Calisan("Melik", "Amaç", "IT");
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+
+            Console.WriteLine("Toplama İşlemi : {0}", Islemler.Topla(50, 60));
+            Console.WriteLine("Çıkarma İşlemi : {0}", Islemler.Cikar(459, 346));
+        }
+        class Calisan
+        {
+            private static int calisanSayisi;
+            public static int CalisanSayisi { get => calisanSayisi; }
+
+            private string Isim;
+            private string Soyisim;
+            private string Departman;
+
+            static Calisan()
+            {
+                calisanSayisi = 0;
+            }
+            public Calisan(string isim, string soyisim, string departman)
+            {
+                this.Isim = isim;
+                this.Soyisim = soyisim;
+                this.Departman = departman;
+                calisanSayisi++;
+            }
+        }
+    }
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
